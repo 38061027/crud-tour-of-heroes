a808faa0a630eda7777a1a1a205e3418c27d6ccf
José Augusto <joseaugustogomessantos12@gmail.com>
2023-05-25 19:52:22 -0300

    Criando lista de heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,12 +1,13 @@
-import { Component , OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Hero } from '../hero.model';
+import { HEROES } from '../mock.heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
-export class HeroesComponent implements OnInit {
+export class HeroesComponent{
 
   hero: Hero = {
     id: 1,
@@ -14,13 +15,11 @@ export class HeroesComponent implements OnInit {
   }
 
 
-  constructor(){}
+  heroes = HEROES
+  selectedHero?: Hero;
 
-
-  ngOnInit(): void {
-
+  onSelect(hero: Hero):void {
+    this.selectedHero = hero
   }
-
-
 
 }
---
+++ src/app/mock.heroes.ts
@@ -0,0 +1,17 @@
+import { Hero } from "./hero.model";
+
+
+export const HEROES: Hero[] = [
+  {id:1 , name: 'Wolverine'},
+  {id:2 , name: 'Homem Aranha'},
+  {id:3 , name: 'Capitão América'},
+  {id:4 , name: 'Homem de Ferro'},
+  {id:5 , name: 'Batman'},
+  {id:6 , name: 'Thor'},
+  {id:7 , name: 'Pantera Negra'},
+  {id:8 , name: 'Capitã Marvel'},
+  {id:9 , name: 'Hulk'},
+  {id:10 , name: 'Superman'},
+];
+
+
