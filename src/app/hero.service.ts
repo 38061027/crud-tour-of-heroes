beb03cf8d20e74d244ca398da9882b0c44de5cdd
José Augusto <joseaugustogomessantos12@gmail.com>
2023-05-26 21:20:33 -0300

    Criando um service

---
+++ src/app/hero.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HEROES } from './mock.heroes';
+import { Hero } from './hero.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  getHeroes(): Hero[]{
+    
+    return HEROES
+
+  }
+
+
+
+}
