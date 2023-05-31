bd7891ba1766391c6ba28cefc50eff6794d862c7
José Augusto <joseaugustogomessantos12@gmail.com>
2023-05-31 16:12:54 -0300

    Adicionando uma navegação e rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import { AppRountingModule } from './app-rounting.module';
 
+//Components
+
+import { MessagesComponent } from './messages/messages.component';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailsComponent } from './hero-details/hero-details.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//Angular Material
+
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
-import { MessagesComponent } from './messages/messages.component';
 import {MatExpansionModule} from '@angular/material/expansion';
 import { MatListModule } from '@angular/material/list';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 
 
@@ -20,7 +28,7 @@ import { MatListModule } from '@angular/material/list';
     AppComponent,
     HeroesComponent,
     HeroDetailsComponent,
-    MessagesComponent
+    MessagesComponent,
   ],
   imports: [
     BrowserModule,
@@ -29,7 +37,10 @@ import { MatListModule } from '@angular/material/list';
     MatToolbarModule,
     MatButtonModule,
     MatExpansionModule,
-    MatListModule
+    MatListModule,
+    AppRountingModule,
+    MatIconModule,
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
