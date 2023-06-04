789aaeec7bc3a589d124f9bea55b0a9f8f4d1111
José Augusto <joseaugustogomessantos12@gmail.com>
2023-06-04 16:06:26 -0300

    Corrigindo erro

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HEROES } from './mock.heroes';
 import { Hero } from './hero.model';
-import { Observable, of, throwError } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 
 @Injectable({
