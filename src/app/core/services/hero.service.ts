5b4d9d6b8273d8eccbb6ef85ce2668d94758ccb5
José Augusto <joseaugustogomessantos12@gmail.com>
2023-06-15 19:17:48 -0300

    Fazendo get de heroes

--- src/app/core/services/hero.service.ts
+++ src/app/core/services/hero.service.ts
@@ -1,49 +1,41 @@
 import { Injectable } from '@angular/core';
-import { HEROES } from './mock.heroes';
+
 import { Hero } from '../models/hero.model';
-import { Observable, of } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { MessageService } from './message.service';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
+  private heroesUrl = `${environment.baseUrl}/heroes`;
 
-  private heroesUrl = 'api/heroes'
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) {}
 
-
-
-
-  constructor( private http: HttpClient, private messageService: MessageService) {}
-
+  // GET /heroes
   getHeroes(): Observable<Hero[]> {
-
-    return this.http.get<Hero[]>(this.heroesUrl);
-
-
-
-    //  const heroes = of(HEROES);
-    //  this.log('fetched heroes')
-    //  return heroes;
-
-
-
+    return this.http
+      .get<Hero[]>(this.heroesUrl)
+      .pipe(tap((heroes) => this.log(`fetched ${heroes.length} hero(es)`)));
   }
 
+  //GET /heroes/id
 
-
-  getHero(id: number): Observable<Hero>{
-
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const hero = HEROES.find((hero) => hero.id == id)!;
-    this.log(`fetched hero id=${id}`)
-    return of(hero);
+  getHero(id: number): Observable<Hero> {
+    return this.http
+      .get<Hero>(`${this.heroesUrl}/${id}`)
+      .pipe(
+        tap((hero) => this.log(`fetched hero id=${id} and name=${hero.name}`))
+      );
   }
 
-  private log(message:string):void{
-    this.messageService.add(`HeroService: ${message}`)
+  private log(message: string): void {
+    this.messageService.add(`HeroService: ${message}`);
   }
-
-
 }
